89e13d2f778572573a7bda1214c87c61aa054545
Seungeun727 <seun2230@gmail.com>
2022-11-05 17:32:00 +0900

    feat: Board 상세 페이지 구성, 특정 id의 게시물 목록 추가

--- src/components/board/BoardDetail.vue
+++ src/components/board/BoardDetail.vue
@@ -1,8 +1,35 @@
 <template>
-  <div>BoardDetail Page</div>
+  <div class="container">
+    <div
+      v-for="post in post"
+      :key="post.board_no"
+      class="content">
+      <div
+        class="content-inner">
+        <span class="content-number">
+          {{ post.board_no }}
+        </span>
+        <span class="content-title">
+          {{ post.board_title }}
+        </span>
+        <span class="content-author">
+          저자: {{ post.board_author }}
+        </span>
+        <div  
+          class="sub-title">
+          {{ post.user_name }}
+          {{ post.createdAt }}
+        </div>
+      </div>
+      <div class="content-text">
+        {{ post.board_text }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
 export default {
   props: {
     id: {
@@ -10,8 +37,56 @@ export default {
       default: 1,
     },
   },
-  
+  computed: {
+    ...mapState('board', ['post']),
+  },
+  created() {
+    this.$store.dispatch("board/getUserPost", { postId: this.id});
+  },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height:700px;
+  padding-top: 70px;
+}
+.content {
+  width: 70%;
+  height: 600px;
+  margin-left: 150px;
+  border-radius:0px 0px 30px 0px;
+  box-shadow: 5px 5px 10px -5px rgba(0, 0, 0, 0.3);
+  .content-inner {
+    padding: 15px;
+    background-color: #333;  
+    color: $white;
+
+    .content-number {
+      display: inline-block;
+      border: 1px solid #fffafa;
+      width: 40px;
+      font-size: $font-size;
+      text-align: center;
+      border-radius: .2rem;
+    }
+  }
+}
+
+.content-title {
+  font-size: $font-size;
+  font-weight: 650;
+  margin: 5px;
+}
+
+.sub-title {
+  color: $light-gray;
+  border-bottom: 1px solid #ccc;
+  padding: 5px 0;
+}
+.post-text {
+  color: black;
+  padding: 10px;
+}
+</style>
--- src/store/board.js
+++ src/store/board.js
@@ -3,12 +3,17 @@ const board = {
   namespaced: true,
   state: () => ({
     posts: [],
+    post: [],
     totalPage: 0,
   }),
   getters: {},
   mutations: {
     setBoardList(state, payload) {
       return state.posts = payload;
+    },
+    setUserPost(state, payload) {
+      console.log(payload);
+      return state.post = payload;
     },
     totalPage(state, payload) {
       return state.totalPage = payload;
@@ -25,6 +30,14 @@ const board = {
         console.log(err.response);
       })
     },
+    getUserPost({commit}, payload) {
+      axios.get(`${process.env.VUE_APP_API_URL}/board/` + payload.postId,
+      ).then((res) => {
+        commit('setUserPost', res.data);        
+      }).catch((err) => {
+        console.log(err.response);
+      })
+    }
   }
 };
 
