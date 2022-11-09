e522614a6df876d3e28d2238bc7ad4b94a162af3
Seungeun727 <seun2230@gmail.com>
2022-11-09 18:44:52 +0900

    feat: Board 게시물 삭제 및 Modal 상태 알림 추가

--- src/components/board/BoardDetailButton.vue
+++ src/components/board/BoardDetailButton.vue
@@ -4,6 +4,26 @@
       :to="{ name: 'BoardEdit', params: { id }}">
       수정
     </router-link>
+    <button
+      type="button"
+      @click="openModal()">
+      삭제
+    </button>
+    <ModalMessage
+      v-if="isShow">
+      <template #header>
+        <h1>삭제</h1>
+      </template>
+      <template #body>
+        <p>{{ message }}</p>
+        <button @click="deletePost()">
+          확인
+        </button>
+        <button @click="cancleModal()">
+          취소
+        </button>
+      </template>
+    </ModalMessage> 
     <button 
       type="button"
       class="btn btn--white"
@@ -14,17 +34,49 @@
 </template>
 
 <script>
+import axios from 'axios';
+import ModalMessage from '../modal/ModalMessage.vue';
 export default {
+  components: {
+    ModalMessage,
+  },
   props: {
     id: {
       type: Number,
       default: 1
     }
   },
+  data() {
+    return {
+      isShow: false,
+      message: '게시물을 삭제하시겠습니까?'
+    }
+  },
   methods: {
     moveList() {
       this.$router.push('/board');
-    }
+    },
+    openModal() {
+      this.isShow = true;
+    },
+    cancleModal() {
+      this.isshow = false;
+    },
+    deletePost() {
+      const postId = this.$route.params.id;
+      axios.post(`${process.env.VUE_APP_API_URL}` + "/board/delete/" + postId, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      .then((res) => {
+        console.log("deletePost Success", res.data);
+        this.message = '게시물이 삭제되었습니다.';
+      })
+      .catch((err) => {
+        console.log("deletePost Error", err.response);
+      })
+    }, 
   }
 }
 </script>
---
+++ src/components/modal/ModalMessage.vue
@@ -0,0 +1,52 @@
+<template>
+  <transition 
+    name="modal" 
+    class="modal">
+    <div class="modal-background">
+      <div class="modal-card">
+        <div class="modal-inner">
+          <div class="modal-header">
+            <slot name="header" />
+          </div>
+          <div class="modal-body">
+            <slot name="body" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+}
+.modal-background {
+  background-color: #0000004b;
+  width: 100%;
+  height: 100%;
+  .modal-card {
+    position: absolute;
+    right: 50%;
+    bottom: 450px;
+    width: 350px;
+    height: 150px;
+    border-radius: 10px;
+    box-shadow: 2px 2px #00000015;
+    border: 1px solid $light-gray;
+    background-color: $white;
+    z-index: 2;
+    padding: 10px;
+  }
+}
+</style>
