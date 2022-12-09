481c3529366cff3df3472b063e6c25781b8f5e15
Seungeun727 <seun2230@gmail.com>
2022-12-09 20:53:02 +0900

    chore: 로그인 상태 유지를 위한 vue3-cookies 설치

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import * as veeValidate from 'vee-validate';
 import App from './App.vue';
 import router from './routes';
 import store from './store';
+import VueCookies from 'vue3-cookies';
 import axios from 'axios';
 
 import 'normalize.css';
@@ -13,6 +14,10 @@ const app = createApp(App)
 app.config.globalProperties.$axios = axios;
 
 app
+  .use(VueCookies, {
+    expiresTimes: "7d",
+    secure: true
+  })
   .use(store)
   .use(router)
   .use(veeValidate)
