9719693275d22a1f155567580148683fee89163e
Seungeun727 <seun2230@gmail.com>
2022-12-10 21:20:24 +0900

    chore: Vuex-persistedstate 설치로 사용자 데이터 유지 및 설정

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from "vuex";
+import createPersistedState from 'vuex-persistedstate';
 import user from './user';
 import board from './board';
 
@@ -6,7 +7,12 @@ const store = createStore({
   modules: {
     user,
     board,
-  }
+  },
+  plugins: [
+    createPersistedState({
+      paths: ['user'],
+    })
+  ],
 });
 
 export default store;
