68b0ad65a620077e70338216c194e0d3f21962d1
Seungeun727 <seun2230@gmail.com>
2022-12-20 13:56:19 +0900

    design: 마이페이지 프로필, 작성 게시물 영역 배치 설정

--- src/views/user/MyPage.vue
+++ src/views/user/MyPage.vue
@@ -1,10 +1,60 @@
 <template>
-  <div>Mypage</div>
+  <div class="my-container">
+    <div class="inner">
+      <aside class="menu">
+        menu
+      </aside>
+      <div class="profile">
+        <UserProfile />
+      </div>
+      <div class="post">
+        <UserBoard />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import UserProfile from '../user/UserProfile.vue';
+import UserBoard from '../user/UserBoard.vue';
 export default {
+  components: {
+    UserProfile,
+    UserBoard,
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@mixin border {
+  border: 1px solid $light-gray;
+  border-radius: 20px;
+}
+.my-container {
+  width: 1400px;
+  height: 1100px;
+  margin-left: 120px;
+  padding-top: 70px;
+  .inner {
+    display: grid;
+    grid-template-columns: 400px 300px;
+    grid-template-rows: 350px 400px;
+    gap: 30px;
+  }
+}
+
+.menu {
+  @include border;
+  grid-area: 1/1/2/2;
+}
+
+.profile {
+  @include border;
+  grid-area: 1/2/2/4;
+}
+
+.post {
+  @include border;
+  grid-area: 2/2/2/4;
+}
+</style>
