d7677655404f93710f583ba30595ae2fa15c355f
Seungeun727 <seun2230@gmail.com>
2022-12-29 15:50:42 +0900

    feat: 마이페이지 회원 기본정보 수정페이지 구조 작성 및 모달 추가

---
+++ src/components/modal/MyPageModalEdit.vue
@@ -0,0 +1,66 @@
+<template>
+  <transition
+    class="modal">
+    <div class="modal-card">
+      <button
+        type="button" 
+        class="btn btn--close"
+        @click.self="closeModal">
+        <FontAwesomeIcon
+          icon="fa-solid fa-xmark" />
+      </button> 
+      <form id="mypage-edit">
+        <div class="form-inner">
+          <div class="main-title">
+            <h2 class="main-text">
+              기본 정보 수정
+            </h2>
+            <span class="sub-text" />
+          </div>
+          <div class="field">
+            <label>이름</label>
+            <input
+              type="text">
+          </div>
+          <div class="field">
+            <label>아이디</label>
+            <input
+              type="text">
+          </div>
+          <div class="field">
+            <label>이메일</label>
+            <input
+              type="text">
+          </div>
+          <div class="btn-group">
+            <button type="button">
+              취소
+            </button>
+            <button type="button">
+              변경
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </transition>
+</template>
+
+<script>
+import { reactive } from 'vue';
+export default {
+  emits: ['close'], 
+  setup(props, context) {
+    const state = reactive({
+      isShow: false,
+    })
+    const closeModal = () => {
+      context.emit('close', state.isShow);
+    };
+    return {
+      closeModal
+    }
+  }
+}
+</script>
+<style lang="scss" scoped></style>
--- src/views/user/MyPageEdit.vue
+++ src/views/user/MyPageEdit.vue
@@ -1,11 +1,81 @@
 <template>
-  <div>MyPageEdit Page</div>
+  <div>
+    <span>기본 프로필</span>
+    <div class="profile">
+      <img class="profile-img">
+      <div class="content">
+        <div class="contnent-inner">
+          <label>이름: </label>
+          <span class="content-info">
+            {{ state.userInfo.user_name }}
+          </span>
+          <button
+            type="button" 
+            @click="showModal()">
+            수정
+          </button>
+        </div>
+        <div class="contnent-inner">
+          <label>아이디: </label>
+          <span class="content-info">
+            {{ state.userInfo.user_id }}
+          </span>
+          <button 
+            type="button" 
+            @click="showModal()">
+            수정
+          </button>
+        </div>
+        <div class="contnent-inner">
+          <label>이메일: </label>
+          <span class="content-info">
+            {{ state.userInfo.user_email }}
+          </span>
+          <button
+            type="button" 
+            @click="showModal()">
+            수정
+          </button>
+        </div>
+      </div>
+    </div>
+    <MyPageModalEdit 
+      v-if="state.isModal"
+      @close="closeChildModal()" />
+  </div>
 </template>
 
 <script>
-export default {
+import MyPageModalEdit from '@/components/modal/MyPageModalEdit.vue';
+import { useStore } from 'vuex';
+import { reactive, computed } from 'vue';
 
-}
+export default {
+  components: {
+    MyPageModalEdit
+  },
+  setup() {
+    const store = useStore();
+    store.dispatch('mypage/getUserInfo');
+    const state = reactive({
+      isModal: false,
+      userInfo: computed(() => store.state.mypage.userInfo)
+    });
+
+    const showModal = () => {
+      state.isModal = true;
+    };
+
+    const closeChildModal = (resultData) => {
+      state.isModal = resultData;
+    }
+    return {
+      state,
+      showModal,
+      closeChildModal,
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
